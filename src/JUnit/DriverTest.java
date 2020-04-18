21391faacbab873ce2330fe7444d90baf4e95c12
Nguyen Tai <taing@Nguyens-MacBook-Pro.local>
2020-04-18 16:15:59 -0700

    a little format

--- src/JUnit/DriverTest.java
+++ src/JUnit/DriverTest.java
@@ -1,20 +1,14 @@
-package junittests;
+package JUnit;
 
 import static org.junit.jupiter.api.Assertions.*;
-
-import java.awt.Robot;
-
-import javax.swing.JFrame;
 
 import org.junit.jupiter.api.Test;
 
 import ISS.Driver;
-import ISS.Wind;
 
 class DriverTest extends Driver {
 	Driver test = new Driver();
-	
-	
+
 	@Test
 	void testDebug() {
 		assertEquals(1, test.debug(), "GUI not created successfully");
